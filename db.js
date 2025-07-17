4c1c58f512449f827c59594b762123914ae7916e
Yash <yashbhardwaj0307@gmail.com>
2025-07-17 22:15:35 +0530

    connected mongoDb

--- db.js
+++ db.js
@@ -1,11 +1,38 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import { Schema,model } from "mongoose";
-// import Model,Schema
-
+import dotenv from 'dotenv';
+dotenv.config();
+export const connectDb=async()=>{
+    try {await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDb connected...");
+    }
+    catch(e){
+        console.error("some error occured",e.message);
+        process.exit(1);
+    }
+}
 const UserSchema= new Schema({
-    name: {type:String , required:true},
-    email: {type:email,required:true, unique:true},
+    Username: {type:String , required:true,unique:true},
     password: {type:String,required:true}
 })
 export const User=model('User',UserSchema);
+const tagSchema = new Schema({
+    title:{type:String,required:true,unique:true}
+})
+export const Tag=model('Tag',tagSchema);
+const linkSchema=new Schema({
+    hash: {type:String,required:true},
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true}
+})
+const contentTypes=['image','video','article','audio'];
+const contentSchema=new Schema({
+    link:{type:String,required:true},
+    type:{type:String,enum:contentTypes,required:true},
+    title:{type:String,required:true},
+    tags:[{type:Types.ObjectId,ref:'Tag'}],
+    userId:{type:Types.ObjectId,ref:'User',required:true}
+})
+
+
+
 
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import express from express;
+import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { User } from "./db.js";
+import { connectDb } from "./db.js";
+await connectDb();
 const app=express();
-
-app.post("/api/v1/signup",(req,res)=>{
-    
+app.use(express.json());
+app.post("/api/v1/signup",async (req,res)=>{
+    try{
+    const Username=req.body.Username;
+    const password=req.body.password;
+    await User.create({
+        Username:Username,
+        password:password})
+        res.json({
+            message:"User created"
+        })
+    }
+    catch(e){
+        res.status(500).json({
+            message:"error:"+e
+        })
+    }
 })
 app.post("/api/v1/signin",(req,res)=>{
 
@@ -21,5 +38,10 @@ app.post("/api/v1/brain/share",(req,res)=>{
 app.get("/api/v1/brain/:shareLink",(req,res)=>{
 
 })
+app.get("/",(req,res)=>{
+    res.send("server is listening")
+})
 
-app.listen(3000);
+app.listen(3000,() => {
+    console.log("Server is running on http://localhost:3000");
+  });
